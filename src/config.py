2330c219f18dfd015f2f8ad24143da13335b8b28
git <mapi@sangut.cl>
2024-07-14 19:56:39 +0200

    fix(inference): prediction result path

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ PROJECT_NAME = "generative-art-project"
 current_p = pathlib.Path(__file__).resolve().parent
 PROJECT_ROOT = current_p.parent
 DATA_DIR = os.path.join(PROJECT_ROOT, "data")
-RESULTS_DIR = os.path.join(PROJECT_ROOT, "results")
-GENERATED_FRAMES_DIR = os.path.join(RESULTS_DIR, "generated_frames")
+RESULTS_DIR = os.path.join(DATA_DIR, "results")
+GENERATED_FRAMES_DIR = os.path.join(RESULTS_DIR, "generated-frames")
 OUTPUT_VIDEO_DIR = os.path.join(RESULTS_DIR, "generated-video")
 
